211198362f459d92b0913142df75366a62b93411
Griffin Novetsky <griffinnovetsky@gmail.com>
2022-05-23 14:05:41 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -16,16 +16,27 @@ def start_game():
  
     # 2. Store a random number as the answer/solution.
     random_number = random.randint(1,100)
-    
+
     # 3. Continuously prompt the player for a guess.
+    guess = int(input("Guess a number between 1 and 100:   "))
+    while guess != random_number:
+
       # a. If the guess greater than the solution, display to the player "It's lower".
+      if guess > random_number:
+        guess = int(input("It's Lower   "))
       # b. If the guess is less than the solution, display to the player "It's higher".
-    
+      elif guess < random_number:
+        guess = int(input("It's Higher   "))
+      
     # 4. Once the guess is correct, stop looping, inform the user they "Got it"
+     
+    print("Got it!")
+
          #and show how many attempts it took them to get the correct number.
     # 5. Save their attempt number to a list.
     # 6. At the end of the game, show the player, 1) their number of attempts, 2) the mean, median, and mode of the saved attempts list.
     # 7. Ask the player if they want to play again.
+
     
     # ( You can add more features/enhancements if you'd like to. )
 
